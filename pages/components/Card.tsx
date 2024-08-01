b1c5e9a8becc88118de5b28817fbfb6313961cff
Gbenga Oshagbemi <oshagbemigbenga@gmail.com>
2024-08-01 01:26:18 +0100

    update

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -25,7 +25,7 @@ const Card: React.FC<CardProps> = ({
       <div className="flex justify-between items-center mb-2">
         <FolderIcon />
         <div className="flex space-x-3">
-          {links.map((link, index) => (
+          {links?.map((link, index) => (
             <a
               key={index}
               href={link.href}
@@ -41,7 +41,7 @@ const Card: React.FC<CardProps> = ({
       <h3 className="text-base pb-3 font-bold mb-2 text-white">{title}</h3>
       <p className="text-white mb-4 text-sm">{description}</p>
       <div className="flex space-x-2 text-gray-500">
-        {technologies.map((tech, index) => (
+        {technologies?.map((tech, index) => (
           <span key={index} className="px-2 py-1 text-xs">
             {tech}
           </span>
