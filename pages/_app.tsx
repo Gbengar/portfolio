7c0d3651a48e84da6eae007aab987417c25ba2b4
Gbenga Oshagbemi <oshagbemigbenga@gmail.com>
2024-08-01 13:18:28 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import "@/styles/globals.css";
 import "animate.css/animate.min.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Gbenga Oshagbemi</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="Website for Gbenga Oshagbemi"
+          content="My Personal portfolio website"
+        />
+        <meta property="og:image" content="/default-image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
